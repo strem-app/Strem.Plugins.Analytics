1ac83959d7e4861a65d6829f9cfa3b8e3ec3c216
LP <grofit1@yahoo.com>
2023-01-16 13:47:10 +0000

    Updating tests to try and fix build

--- src/Strem.Plugins.Analytics.UnitTests/Analytics/AnalyticsPluginStartupTests.cs
+++ src/Strem.Plugins.Analytics.UnitTests/Analytics/AnalyticsPluginStartupTests.cs
@@ -1,11 +1,33 @@
-﻿namespace Strem.Plugins.Analytics.UnitTests.Analytics;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Persistity.Encryption;
+using Strem.Core.Events.Bus;
+using Strem.Core.State;
+using Strem.Plugins.Analytics.Plugin;
+using Strem.Plugins.Analytics.Services.Database;
+using Strem.Plugins.Analytics.Services.Metrics;
+using Strem.Plugins.Analytics.Services.Settings;
+
+namespace Strem.Plugins.Analytics.UnitTests.Analytics;
 
 public class AnalyticsPluginStartupTests
 {
     [Fact]
-    public void should_correctly_setup_plugin()
+    public async Task should_correctly_setup_plugin()
     {
-        // Todo
-        Assert.True(true);
+        var mockEventBus = new Mock<IEventBus>();
+        var mockAppState = new Mock<IAppState>();
+        var mockEncryptor = new Mock<IEncryptor>();
+        var mockAnalyticsRegistry = new Mock<IAnalyticsSettingsRegistry>();
+        var mockAnalyticsDatabase = new Mock<IAnalyticsDatabase>();
+        var mockServiceProvider = new Mock<IServiceProvider>();
+        var mockLogger = new Mock<ILogger<AnalyticsPluginStartup>>();
+        var mockAnalyticsComponentRegistry = new Mock<IAnalyticsComponentRegistry>();
+
+        var analyticsPluginStartup = new AnalyticsPluginStartup(mockEventBus.Object, mockAppState.Object,
+            mockEncryptor.Object, mockAnalyticsRegistry.Object, mockAnalyticsDatabase.Object,
+            mockServiceProvider.Object, mockLogger.Object, mockAnalyticsComponentRegistry.Object);
+
+        await analyticsPluginStartup.SetupPlugin();
     }
 }
