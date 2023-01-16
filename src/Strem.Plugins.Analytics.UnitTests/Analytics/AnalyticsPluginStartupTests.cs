a2380a1c273d255834b5c52f366d19dfa6d536f4
LP <grofit1@yahoo.com>
2023-01-16 13:08:43 +0000

    Updated to try and fix build

---
+++ src/Strem.Plugins.Analytics.UnitTests/Analytics/AnalyticsPluginStartupTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Strem.Plugins.Analytics.UnitTests.Analytics;
+
+public class AnalyticsPluginStartupTests
+{
+    [Fact]
+    public void should_correctly_setup_plugin()
+    {
+        // Todo
+        Assert.True(true);
+    }
+}
